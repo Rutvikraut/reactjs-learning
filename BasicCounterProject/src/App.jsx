d9a0e7593e7470bd2041851798f50f8ca0aa7368
Rutvik Raut <rutvikraut567@gmail.com>
2024-09-10 11:21:01 +0530

    Counter Question

--- BasicCounterProject/src/App.jsx
+++ BasicCounterProject/src/App.jsx
@@ -9,6 +9,24 @@ function App() {
   const removeCounter=()=>{
     counter<=0?setCounter(0):setCounter(counter-1)
   }
+
+  // For interview
+  const addMulitple=()=>{
+    setCounter((prevCounter)=>prevCounter+1)
+    setCounter((prevCounter)=>prevCounter+1)
+    setCounter((prevCounter)=>prevCounter+1)
+    setCounter((prevCounter)=>prevCounter+1)
+  }
+  const RemoveMulitple=()=>{
+    if(counter<=0){
+      setCounter(0)
+    }else{
+      setCounter((prevCounter)=>prevCounter-1)
+      setCounter((prevCounter)=>prevCounter-1)
+      setCounter((prevCounter)=>prevCounter-1)
+      setCounter((prevCounter)=>prevCounter-1)
+    }
+  }
   return (
     <>
     <h1>Count is {counter}</h1>
@@ -17,6 +35,8 @@ function App() {
           Add Counter
         </button>
         <button onClick={removeCounter}>Remove Count</button>
+        <button onClick={addMulitple}>Add Count by 4</button>
+        <button onClick={RemoveMulitple}>Remove Count by 4</button>
       </div>
     </>
   )
